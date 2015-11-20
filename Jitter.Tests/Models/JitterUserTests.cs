ca951af3e21546fab9995e966ae3e49caeb37ab4
wfhutch <wfh2727@gmail.com>
2015-11-20 10:10:56 -0600

    Added tests for Following and Jots

--- Jitter.Tests/Models/JitterUserTests.cs
+++ Jitter.Tests/Models/JitterUserTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Jitter.Models;
+using System.Collections.Generic;
 
 namespace Jitter.Tests.Models
 {
@@ -31,7 +32,40 @@ namespace Jitter.Tests.Models
             Assert.AreEqual("Adam", a_user.FirstName);
             Assert.AreEqual("Sandler", a_user.LastName);
             Assert.AreEqual("https://google.com", a_user.Picture);
+        }
 
+        [TestMethod]
+        public void JitterUserEnsureUserHasJots()
+        {
+            //Arrange
+            List<Jot> list_of_jots = new List<Jot>
+            {
+                new Jot { Content = "blah" },
+                new Jot { Content = "blah part 2!" }
+            };
+            JitterUser a_user = new JitterUser { Handle = "adam1", Jots = list_of_jots};
+
+            //Act
+            List<Jot> actual_jots = a_user.Jots;
+            //Assert
+            CollectionAssert.AreEqual(list_of_jots, actual_jots);
+        }
+
+        [TestMethod]
+        public void JitterUserEnsureUserFollowsOthers()
+        {
+            //Arrange
+            List<JitterUser> list_of_users = new List<JitterUser>
+            {
+                new JitterUser { Handle = "blah" },
+                new JitterUser { Handle = "blah2" }
+            };
+            JitterUser a_user = new JitterUser { Handle = "adam1", Following = list_of_users };
+
+            //Act
+            List<JitterUser> actual_users = a_user.Following;
+            //Assert
+            CollectionAssert.AreEqual(list_of_users, actual_users);
         }
     }
 }

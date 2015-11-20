316a3000a33b37effd964e25837f33a608a8f0c1
wfhutch <wfh2727@gmail.com>
2015-11-20 12:24:54 -0600

    Added JitterContext and JitterRepository classes

---
+++ Jitter/Models/JitterContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Jitter.Models
+{
+    public class JitterContext : DbContext  // Have to be using System.Data.Entity to inherit from DbContext
+    {
+        // IDbSet, IQueryable
+        public DbSet<JitterUser> JitterUsers { get; set; }  // Creates JitterUser table in the database.
+        public DbSet<Jot> Jots { get; set; }  // Allows access to jots without going through user.
+
+    }
+}

f31aaa8146975442e82e17070b86ca46f85ffedf
wfhutch <wfh2727@gmail.com>
2015-11-24 09:03:52 -0600

    Added comments

--- Jitter/Models/JitterContext.cs
+++ Jitter/Models/JitterContext.cs
@@ -8,6 +8,7 @@ namespace Jitter.Models
 {
     public class JitterContext : DbContext 
     {
+        // This class provides the functionality to talk to database.
         // IDbSet, IQueryable
         public virtual DbSet<JitterUser> JitterUsers { get; set; }
         public DbSet<Jot> Jots { get; set; }

026571cc71d4aacd2e08fbbef1b8cdd7fd9e7890
wfhutch <wfh2727@gmail.com>
2015-12-09 12:23:46 -0600

    Added Unit Test for TestController

---
+++ Jitter.Tests/Controllers/TestControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Jitter.Controllers;
+
+namespace Jitter.Tests.Controllers
+{
+    [TestClass]
+    public class TestControllerTests
+    {
+        [TestMethod]
+        public void TestControllerEnsureICanCallItAction()
+        {
+            TestController my_controller = new TestController();
+            string expected_output = "Hello, It's Me!";
+
+            string actual_output = my_controller.Get();
+
+            Assert.AreEqual(expected_output, actual_output);
+        }
+    }
+}

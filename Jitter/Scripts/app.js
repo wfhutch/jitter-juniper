9532f2c9ec88415fe2990ed38e63467752ebb12f
wfhutch <wfh2727@gmail.com>
2015-12-10 14:29:35 -0600

    Added Angular and app.js files

---
+++ Jitter/Scripts/app.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("jitter", []);
+
+app.controller("TestController", ["$scope", "$http", function ($scope, $http) {
+    $scope.test = "test variable";
+
+    $scope.hello = function () {
+        //$scope.test = "Hello World";
+
+        $http.get("/api/Test")
+            .success(function (data) {
+                $scope.test = data;
+            })
+            .error(function (error) { alert(error.error) });
+    }
+}]);
